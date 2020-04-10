0a17dcf09592a8f58ec05e0136f4333629ad2cd9
Nishant Dutta <nishant.dutta@cavisson.com>
2020-04-10 12:33:21 +0530

    Removed firebase dependencies and commented related code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { PostData } from './post-data';
 import { Observable, from } from 'rxjs';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { AngularFireStorage } from '@angular/fire/storage';
-import { finalize } from 'rxjs/operators';
+// import { AngularFireStorage } from '@angular/fire/storage';
+// import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +11,14 @@ import { finalize } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items: Observable<any[]>;
+  // items: Observable<any[]>;
 
-  constructor(private firestorage: AngularFireStorage) {
-  }
+  // constructor(private firestorage: AngularFireStorage) {
+  // }
 
-  ngOnInit(){
-    this.resetForm();
-  }
+  // ngOnInit(){
+  //   this.resetForm();
+  // }
 
   title = 'blog-post';
   postData: PostData[] = [
@@ -32,61 +32,61 @@ export class AppComponent {
     new PostData("assets/high-tech2.jpg", "Human evolution", "Dinasaurs the most deadly of all, could they have let humans survive if they existed",4,false),
 ];
 
-  formTemplate = new FormGroup({
-    caption: new FormControl('',Validators.required),
-    category: new FormControl,
-    imageUrl: new FormControl('',Validators.required)
-  })
+  // formTemplate = new FormGroup({
+  //   caption: new FormControl('',Validators.required),
+  //   category: new FormControl,
+  //   imageUrl: new FormControl('',Validators.required)
+  // })
 
-  imgSrc: string = "assets/abstract.jpg";
-  selectedImage: File = null;
-  isSubmitted: boolean = false;
+  // imgSrc: string = "assets/abstract.jpg";
+  // selectedImage: File = null;
+  // isSubmitted: boolean = false;
 
-  showPreview(event){
-    console.log("Value in showPreview event:",event);
-    if(event.target.files && event.target.files[0]){
-      const reader = new FileReader();
-      reader.onload = (e: any) => this.imgSrc = e.target.result;
-      reader.readAsDataURL(event.target.files[0]);
-      this.selectedImage = event.target.files[0];
-    }
-    else{
-      this.imgSrc = "assets/abstract.jpg";
-      this.selectedImage = null;
-    }
-  }
+  // showPreview(event){
+  //   console.log("Value in showPreview event:",event);
+  //   if(event.target.files && event.target.files[0]){
+  //     const reader = new FileReader();
+  //     reader.onload = (e: any) => this.imgSrc = e.target.result;
+  //     reader.readAsDataURL(event.target.files[0]);
+  //     this.selectedImage = event.target.files[0];
+  //   }
+  //   else{
+  //     this.imgSrc = "assets/abstract.jpg";
+  //     this.selectedImage = null;
+  //   }
+  // }
 
-  onSubmit(formValue){
-    this.isSubmitted = true;
-    if(this.formTemplate.valid){
-      var filePath = `${formValue.category}/${this.selectedImage.name}_${new Date().getTime()}`;
-      const fileRef = this.firestorage.ref(filePath);
-      this.firestorage.upload(filePath,this.selectedImage).snapshotChanges()
-      .pipe(
-        finalize(() => {
-          fileRef.getDownloadURL().subscribe((url) => {
-            formValue['imageUrl'] = url; 
-            this.resetForm();
-          })
-        })
-      )
-      .subscribe();
-    }
-  }
+  // onSubmit(formValue){
+  //   this.isSubmitted = true;
+  //   if(this.formTemplate.valid){
+  //     var filePath = `${formValue.category}/${this.selectedImage.name}_${new Date().getTime()}`;
+  //     const fileRef = this.firestorage.ref(filePath);
+  //     this.firestorage.upload(filePath,this.selectedImage).snapshotChanges()
+  //     .pipe(
+  //       finalize(() => {
+  //         fileRef.getDownloadURL().subscribe((url) => {
+  //           formValue['imageUrl'] = url; 
+  //           this.resetForm();
+  //         })
+  //       })
+  //     )
+  //     .subscribe();
+  //   }
+  // }
 
-  get formControls(){
-    return this.formTemplate['controls']
-  }
+  // get formControls(){
+  //   return this.formTemplate['controls']
+  // }
 
-  resetForm(){
-    this.formTemplate.reset();
-    this.formTemplate.setValue({
-      caption: '',
-      imageUrl: '',
-      category: 'Animal'
-    });
-    this.imgSrc = "assets/abstract.jpg";
-    this.isSubmitted = false;
-    this.selectedImage = null;
-  }
+  // resetForm(){
+  //   this.formTemplate.reset();
+  //   this.formTemplate.setValue({
+  //     caption: '',
+  //     imageUrl: '',
+  //     category: 'Animal'
+  //   });
+  //   this.imgSrc = "assets/abstract.jpg";
+  //   this.isSubmitted = false;
+  //   this.selectedImage = null;
+  // }
 }
